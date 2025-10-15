310f104ef4816e760c8ca647cfdbc9edb7e6ce7a
Sadovoi Grigorii <niuitmo368748@yandex.ru>
2025-10-15 16:44:57 +0300

    feat(model): extend City entity with additional fields

--- backend/src/main/java/org/itmo/domain/City.java
+++ backend/src/main/java/org/itmo/domain/City.java
@@ -2,22 +2,114 @@ package org.itmo.domain;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-@Entity @Table(name="city")
+@Entity
+@Table(name = "city")
 public class City {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank private String name;
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
 
-    @NotNull
+    @Positive(message = "Area must be greater than 0")
+    private int area;
+
+    @NotNull(message = "Population cannot be null")
+    @Positive(message = "Population must be greater than 0")
+    private Long population;
+
+    private boolean capital;
+
+    private Integer metersAboveSeaLevel;
+
+    @Positive(message = "Telephone code must be greater than 0")
+    @Max(value = 100000, message = "Telephone code cannot exceed 100000")
+    private Integer telephoneCode;
+
+    @NotNull(message = "Climate type must be specified")
     @Column(name = "CLIMATE", columnDefinition = "climate")
     @Convert(converter = ClimatePgEnumConverter.class)
     private Climate climate;
 
-    public Long getId(){return id;} public void setId(Long id){this.id=id;}
-    public String getName(){return name;} public void setName(String name){this.name=name;}
-    public Climate getClimate(){return climate;} public void setClimate(Climate climate){this.climate=climate;}
+    @Column(name = "GOVERNMENT", columnDefinition = "government")
+    @Convert(converter = GovernmentPgEnumConverter.class)
+    private Government government;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public Government getGovernment() {
+        return government;
+    }
+
+    public void setGovernment(Government government) {
+        this.government = government;
+    }
+
+    public Integer getTelephoneCode() {
+        return telephoneCode;
+    }
+
+    public void setTelephoneCode(Integer telephoneCode) {
+        this.telephoneCode = telephoneCode;
+    }
+
+    public Integer getMetersAboveSeaLevel() {
+        return metersAboveSeaLevel;
+    }
+
+    public void setMetersAboveSeaLevel(Integer metersAboveSeaLevel) {
+        this.metersAboveSeaLevel = metersAboveSeaLevel;
+    }
+
+    public boolean isCapital() {
+        return capital;
+    }
+
+    public void setCapital(boolean capital) {
+        this.capital = capital;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Climate getClimate() {
+        return climate;
+    }
+
+    public void setClimate(Climate climate) {
+        this.climate = climate;
+    }
 }
